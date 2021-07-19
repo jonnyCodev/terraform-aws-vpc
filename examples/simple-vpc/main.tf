3bdf0ba7565385495c0bc4d4a2c21d94d5662360
guyeisenkot <68634672+guyeisenkot@users.noreply.github.com>
2021-07-19 11:14:28 +0300

    cidr-change

--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -14,7 +14,7 @@ module "vpc" {
   source = "../../"
 
   name = "simple-example"
-  cidr = "10.0.0.0/16"
+  cidr = "0.0.0.0/0"
 
   azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
