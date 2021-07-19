46455c536c55a3ad1895453b571ed08ccc9db522
guyeisenkot <68634672+guyeisenkot@users.noreply.github.com>
2021-07-19 11:35:26 +0300

    Update examples/ipv6/main.tf
    Co-authored-by: bridgecrew-dev[bot] <83754225+bridgecrew-dev[bot]@users.noreply.github.com>

--- examples/ipv6/main.tf
+++ examples/ipv6/main.tf
@@ -168,7 +168,14 @@ resource "aws_flow_log" "vpcflowlogs" {
   }
 }
 
-resource "aws_s3_bucket" "flowbucket" {
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
   bucket        = "${local.resource_prefix.value}-flowlogs"
   force_destroy = true
 
